ebb5d48bc209ad38a7a828d1a53160c5f1e9d2b9
Arturo Aguilar Lopez <aguilarlopez18@gmail.com>
2021-02-11 11:57:56 -0300

    agrego test's en algoritmo personalizado

--- src/main/test/tests/BloqueAlgoritmoTest.java
+++ src/main/test/tests/BloqueAlgoritmoTest.java
@@ -1,6 +1,5 @@
 package tests;
-import bloque.BloqueAlgoritmo;
-import bloque.BloqueMovimiento;
+import bloque.*;
 import movimiento.MovAbajo;
 import movimiento.MovArriba;
 import movimiento.MovDerecha;
@@ -14,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BloqueAlgoritmoTest {
     @Test
-        public void test01CreoUnBloqueAlgoritmoPersonalizadoRecorriendoUnCuadradoYDeberiaVolverALaPosicionOriginal() {
+    public void test01CreoUnBloqueAlgoritmoPersonalizadoRecorriendoUnCuadradoYDeberiaVolverALaPosicionOriginal() {
             Tablero tablero = new Tablero();
             BloqueAlgoritmo algoritmoPersonalizado = new BloqueAlgoritmo();
            /* MovDerecha movDerecha = new MovDerecha();
@@ -25,21 +24,87 @@ public class BloqueAlgoritmoTest {
             BloqueMovimiento bloqDerecha = new BloqueMovimiento(new MovDerecha());
             BloqueMovimiento bloqArriba = new BloqueMovimiento(new MovArriba());
             BloqueMovimiento bloqIzquierda = new BloqueMovimiento(new MovIzquierda());
-            BloqueMovimiento bloqAbajo = new BloqueMovimiento(new MovAbajo());
 
+            //Voy a la posicion (0,1)
             algoritmoPersonalizado.agregarBloque(bloqDerecha);
+            //Voy a la posicion (1,1)
             algoritmoPersonalizado.agregarBloque(bloqArriba);
+            //Voy a la posicion (0,1)
             algoritmoPersonalizado.agregarBloque(bloqIzquierda);
-            algoritmoPersonalizado.agregarBloque(bloqAbajo);
+
 
             Posicion posicion = new Posicion(0,0);
             Personaje unPj = new Personaje(posicion, tablero);
 
 
             algoritmoPersonalizado.ejecutar(unPj);
-            Posicion posicion1 = new Posicion(0,0);
+            Posicion posicion1 = new Posicion(0,1);
 
             assertEquals(true, posicion.esIgualA(posicion1));
-        }
+    }
+
+    @Test
+    public void test02PrueboInsertarTodosLosBloquesEnElAlgoritmoPersonalizado(){
+        Tablero tablero = new Tablero();
+        BloqueAlgoritmo algoritmoPersonalizado = new BloqueAlgoritmo();
+
+        BloqueLapizAbajo bloqLapizAbajo = new BloqueLapizAbajo();
+        BloqueLapizArriba bloqLapizArriba = new BloqueLapizArriba();
+        BloqueMovimiento bloqDerecha = new BloqueMovimiento(new MovDerecha());
+        BloqueMovimiento bloqArriba = new BloqueMovimiento(new MovArriba());
+        BloqueMovimiento bloqIzquierda = new BloqueMovimiento(new MovIzquierda());
+        BloqueMovimiento bloqAbajo = new BloqueMovimiento(new MovAbajo());
+        BloqueRepetirTresVeces bloqueRepTres = new BloqueRepetirTresVeces(new MovArriba());
+        BloqueRepetirDosVeces bloqueRepDos = new BloqueRepetirDosVeces(new MovDerecha());
+
+
+        //(0,3)
+        algoritmoPersonalizado.agregarBloque(bloqueRepTres);
+
+        //(2,3)
+        algoritmoPersonalizado.agregarBloque(bloqueRepDos);
+
+        algoritmoPersonalizado.agregarBloque(bloqLapizArriba);
+        algoritmoPersonalizado.agregarBloque(bloqLapizAbajo);
+
+        //(3,3)
+        algoritmoPersonalizado.agregarBloque(bloqDerecha);
+
+        //(3,4)
+        algoritmoPersonalizado.agregarBloque(bloqArriba);
+
+        //(6,4)
+        algoritmoPersonalizado.agregarBloque(new BloqueRepetirTresVeces(new MovDerecha()));
+
+        //(6,3)
+        algoritmoPersonalizado.agregarBloque(bloqAbajo);
+
+        //(5,3)
+        algoritmoPersonalizado.agregarBloque(bloqIzquierda);
+
+        Posicion posicion = new Posicion(0,0);
+        Personaje unPj = new Personaje(posicion, tablero);
+
+
+        algoritmoPersonalizado.ejecutar(unPj);
+        Posicion posicion1 = new Posicion(5,3);
+
+        assertEquals(true, posicion.esIgualA(posicion1));
+
+    }
+
+    @Test
+    public void test03CreoUnAlgoritmoPersonalizadoSinBloqueYNoDeberiaHacerNada() {
+        Tablero tablero = new Tablero();
+        BloqueAlgoritmo algoritmoPersonalizado = new BloqueAlgoritmo();
+
+        Posicion posicion = new Posicion(1,1);
+        Personaje unPj = new Personaje(posicion, tablero);
+
+        algoritmoPersonalizado.ejecutar(unPj);
+        Posicion posicion1 = new Posicion(1,1);
+
+        assertEquals(true, posicion.esIgualA(posicion1));
+    }
 
 }
